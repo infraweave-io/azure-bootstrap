ea902c299fb5e329fd759626835597abcde847a1
InfiniteTabs <18383533+InfiniteTabs@users.noreply.github.com>
2025-05-14 07:35:53 +0200

    docs: modify environment to dev in locals.tf to align with readme

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 
 locals {
-  environment             = "prod"
+  environment             = "dev"
   central_subscription_id = "00000000-0000-0000-0000-000000000000"
   tags = {
     #Environment = local.environment
